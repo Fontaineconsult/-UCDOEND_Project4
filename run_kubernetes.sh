ae2dd843ac71c0de60d6d06754867da5355b5892
Daniel Fontaine <fontaine@sfsu.edu>
2021-01-02 01:26:52 -0800

    First

---
+++ run_kubernetes.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+export dockerpath=fontaineconsult/project4:firsttry
+
+# Step 2
+kubectl run project4 --image=$dockerpath --port=8000
+
+
+# Step 3:
+kubectl get deployments
+
+# Step 4:
+kubectl port-forward project4	 8080:80
+
